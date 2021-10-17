de5624754e241897561bac1a1a82cfe50fed1b87
Morgan Rosenkranz <rosenkranz1999@gmai.com>
2021-10-17 12:45:26 -0400

    added faked acceleration physics yay!

--- main.lisp
+++ main.lisp
@@ -1,35 +1,56 @@
 (require :cl-raylib)
 
 (defpackage :raylib-user
- (:use :cl :raylib))
+  (:use :cl :raylib :3d-vectors))
 
 (in-package :raylib-user)
 
-(defparameter *player-pos* (make-vector2 :x 100 :y 100))
-(defparameter *player-size* (make-vector2 :x 30 :y 100))
+(defclass player ()
+  ((pos :initform (vec2 0 0) :accessor :pos)
+   (acc :initform (vec2 0 0) :accessor :acc)
+   (vel :initform (vec2 0 0) :accessor :vel)))
+
+;; create our player
+(defparameter *player* (make-instance 'player))
+
+(defun update-player ()
+  (let ((idir (vec2 0 0)))
+    (cond
+      ((is-key-down +key-a+) (setf (vx idir) -2))
+      ((is-key-down +key-d+) (setf (vx idir) 2)))
+    ;; TODO: if grounded then we want to add friction
+    (setf (:acc *player*) idir)
+    ;; friction
+    (if (> (vlength (:vel *player*)) 0)
+        (nv- (:acc *player*) (nvunit (vec2 (vx (:vel *player*)) 0)))))
+  (nvlimit (nv+ (:vel *player*) (:acc *player*)) 10)
+  (nv+ (:pos *player*) (:vel *player*)))
 
 (defun draw-player ()
   (draw-rectangle-lines
-      (vector2-x *player-pos*)
-      (vector2-y *player-pos*)
-      (vector2-x *player-size*)
-      (vector2-y *player-size*)
-      +darkgreen+))
+    (rational (vx (:pos *player*)))
+    (rational (vy (:pos *player*)))
+    20
+    100
+    +darkgreen+))
 
 (defun game-loop ()
+  (update-player)
   (with-drawing
-    
     (clear-background +raywhite+)
     (draw-fps 20 20)
+    (draw-text (format nil "axis0: ~a" (get-gamepad-axis-movement 1 0))
+               20 50 20 +lightgray+)
     (draw-player)))
 
 (defun main ()
- (let ((screen-width 800)
-       (screen-height 450))
-   (with-window (screen-width screen-height "mjr game")
-               (set-target-fps 60) ; Set our game to run at 60 FPS
-               (loop
-                 (if (window-should-close) (return))
-                 (game-loop)))))
+  (let ((screen-width 800)
+        (screen-height 450))
+    (with-window (screen-width screen-height "mjr game")
+      (set-target-fps 60) ; Set our game to run at 60 FPS
+      (loop
+        (if (window-should-close) (return))
+        (game-loop)))))
 
 (main)
+
